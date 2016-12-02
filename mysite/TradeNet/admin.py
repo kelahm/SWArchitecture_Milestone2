ae3514bb64538e2e39ccc424785839f77540056c
Ricky Arendall <ra529@msstate.edu>
2016-12-02 14:54:52 -0600

    Added Database tables in Models.py and registered in Admin.py

--- mysite/TradeNet/admin.py
+++ mysite/TradeNet/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Company)
+admin.site.register(Stock)
+admin.site.register(Transaction)
+admin.site.register(User)
+admin.site.register(OwnedStock)
+admin.site.register(History)
--- mysite/TradeNet/models.py
+++ mysite/TradeNet/models.py
@@ -3,3 +3,42 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+'''
+class User (models.Model):
+    userID = models.IntegerField(default=0) #Integer key for database
+    balance = models.FloatField(default=0) #Float/real (only two decimal places allowed) for amount of money
+    PnL = models.FloatField(default=0) #float/real total profit, negative value indicates loss
+    portfolio = Portfolio() #Portfolio object to manage transactions and owned stock
+'''
+
+class Company(models.Model):
+    name = models.CharField(max_length=200)
+    finGrade = models.CharField(max_length=10)
+    currentRev = models.FloatField(default=0)
+    profit = models.FloatField(default=0)
+
+class Stock(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    ticker = models.CharField(max_length=5)
+    lastPrice = models.FloatField(default=0)
+    exchange = models.FloatField(default=0)
+    dailyNetChange = models.FloatField(default=0)
+
+class Transaction(models.Model):
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    price = models.FloatField(default=0)
+    time = models.DateTimeField('time of transaction')
+    quantity = models.IntegerField(default=1)
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    balance = models.FloatField(default=0.00)
+    profit = models.FloatField(default=0.00)
+
+class OwnedStock(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
+
+class History(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    transaction = models.ForeignKey(Transaction, on_delete=models.CASCADE)
